f6a46538cd11389bebc4afbfb409d2855a91370f
Bannon <mbannon@anl.gov>
2021-01-01 18:57:06 -0600

    deleted the way I added the data for function build table

--- static/js/app.js
+++ static/js/app.js
@@ -3,20 +3,42 @@ var tableData = data;
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
-var button = d3.select("#filter-btn");
-var inputField1 = d3.select("#datetime");
-var inputField2 = d3.select("#city");
-var resetbtn = d3.select("#reset-btn");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-var populate = (dataInput) => {
+// Function build table
+function buildTable(data){
+    // First, clear out existing data
+    tbody.html("");
+    data.forEach(dataRow => {
+        console.table(dataRow);
+        let row = tbody.append("tr");
 
-	dataInput.forEach(ufo_sightings => {
-		var row = tbody.append("tr");
-		columns.forEach(column => row.append("td").text(ufo_sightings[column])
-		)
-	});
+       console.table(Object.values(dataRow));
+       Object.values(dataRow).forEach((val) => {
+           let cell = row.append("td");
+           cell.text(val);
+       });
+    });
 }
 
-//Populate table
-populate(data);
+// Event that calls a function name is handleClick and use d3 to prevent form for refreshing
+function handleClick(){
+    d3.event.preventDefault() // prevent the form from refreshing the page
+    
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
+
+    // Check to see if a date was entered and filter the data using that date
+    if (date){
+        // Apply filter to the table data to only keep the 
+        // rows where the datetime value matches the filter value
+        filterData = filterData.filter((row) => row.datetime === date);
+
+    }
+
+    buildTable(filterData);
+}
+
+
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+buildTable(tableData);
