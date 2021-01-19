20363a4faba3162540e67a69188b2c91210f5fee
Michelle <michellerbannon@gmail.com>
2021-01-19 14:24:35 -0600

    tried to figure out why my selection didn't work.

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
-// Function build table
+// Function to build table I worked with my tutor on this
 function buildTable(data){
     // First, clear out existing data
     tbody.html("");
@@ -27,14 +27,13 @@ function handleClick(){
     let date = d3.select("#datetime").property("value");
     let filterData = tableData;
 
-    // Check to see if a date was entered and filter the data using that date
     if (date){
-        // Apply filter to the table data to only keep the 
-        // rows where the datetime value matches the filter value
+// Filter data to select by using identity operator to verify both dates are the same
         filterData = filterData.filter((row) => row.datetime === date);
-
     }
 
+// build the table based on the dates selected 
+// THIS PART DOESN'T WORK not sure why
     buildTable(filterData);
 }
 
